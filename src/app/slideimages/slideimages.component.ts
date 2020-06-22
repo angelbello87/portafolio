6833340e99fe1c67cb380206e00b3133ff9b3cc3
Angel <angelbello87@yahoo.com>
2020-06-22 17:54:45 -0500

    add change pic option

--- src/app/slideimages/slideimages.component.ts
+++ src/app/slideimages/slideimages.component.ts
@@ -14,21 +14,57 @@ export class SlideimagesComponent implements OnInit {
   @Input() imageCount:number;
 
   imageURL: Array<string>;
+  pic = "";
+  imageindex:number;
 
   constructor() { 
     this.imageURL = new Array<string>();
+    this.imageindex = 0;
   }
 
   ngOnInit(): void {
     for( let i = 0; i < this.imageCount; i++)
     {
       let p = i + 1;
-      let url: string = "../"+this.imageRoute + "photo" + p + ".png";
+      let url: string = this.imageRoute + "photo" + p + ".png";
       this.imageURL.push(url); 
     }
     
-    
+    this.pic = this.imageURL[this.imageindex];    
     console.log(this.imageURL);
   }
 
+  public ChangeNextPic():void
+  {
+    this.imageindex += 1;
+    if(this.imageindex >= this.imageURL.length)
+    {
+      this.pic = this.imageURL[0];
+      this.imageindex = 0;
+    }
+    else
+    {      
+      this.pic = this.imageURL[this.imageindex];
+    }
+  }
+
+  public ChangePrevPic():void
+  {
+    this.imageindex -= 1; 
+    if(this.imageindex < 0)
+    {
+      this.pic = this.imageURL[this.imageURL.length-1];
+      this.imageindex = this.imageURL.length - 1;
+    }
+    else
+    {      
+      this.pic = this.imageURL[this.imageindex];
+    }
+  }
+
+  public ChangePic(picurl:string):void
+  {
+    this.pic = picurl;
+    this.imageindex = this.imageURL.findIndex( i => i == picurl)
+  }
 }
