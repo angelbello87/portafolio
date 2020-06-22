a4955754e847d3af7de354b9fb030da680ad2eaa
Angel <angelbello87@yahoo.com>
2020-06-22 16:37:29 -0500

    change images

--- src/app/slideimages/slideimages.component.ts
+++ src/app/slideimages/slideimages.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+
 @Component({
   selector: 'app-slideimages',
   templateUrl: './slideimages.component.html',
@@ -9,9 +10,25 @@ export class SlideimagesComponent implements OnInit {
 
   @Input() title:string;
   @Input() comment:string;
-  constructor() { }
+  @Input() imageRoute:string;
+  @Input() imageCount:number;
+
+  imageURL: Array<string>;
+
+  constructor() { 
+    this.imageURL = new Array<string>();
+  }
 
   ngOnInit(): void {
+    for( let i = 0; i < this.imageCount; i++)
+    {
+      let p = i + 1;
+      let url: string = "../"+this.imageRoute + "photo" + p + ".png";
+      this.imageURL.push(url); 
+    }
+    
+    
+    console.log(this.imageURL);
   }
 
 }
